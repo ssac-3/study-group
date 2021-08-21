0d75ac0c5f86eccd48102a661abb9e58ceb9c0c9
Seung-wan <tmddhks0104@gmail.com>
2021-08-21 22:26:53 +0900

    for test

---
+++ 1.Array/2.js
@@ -0,0 +1,16 @@
+//  Array In JavaScript
+
+// 비어 있는 리스트 선언
+const js_list_empty = [];
+console.log(js_list_empty);
+
+// 1, 2, 3, 4, 5의 요소를 가지는 리스트 선언
+const js_list = [1, 2, 3, 4, 5];
+console.log(js_list);
+
+// 0을 10개 가지는 리스트 초기화
+const js_list_zeros_1 = [];
+for (let i = 0; i < 10; i += 1) {
+  js_list_zeros_1.push(0);
+}
+console.log(js_list_zeros_1);
