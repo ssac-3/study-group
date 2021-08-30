28227c3f469f1ec1973e951a0f0bd82458454e5f
Seung-wan <tmddhks0104@gmail.com>
2021-08-31 01:15:20 +0900

    update hashTable

--- 1.Array/3.js
+++ 1.Array/3.js
@@ -2,17 +2,33 @@
 // 입력값 : nums=[2,7,10,19]. target = 9
 // 출력값 : [0,1]
 
-function findSumOfTwo(nums, target) {
-  const answer = [];
-  const len = nums.length;
-  for (let i = 0; i < len; i += 1) {
-    for (let j = i + 1; j < len; j += 1) {
-      if (nums[i] + nums[j] === target) {
-        answer.push(i, j);
-        return answer;
-      }
+// function findSumOfTwo(nums, target) {
+//   const answer = [];
+//   const len = nums.length;
+//   for (let i = 0; i < len; i += 1) {
+//     for (let j = i + 1; j < len; j += 1) {
+//       if (nums[i] + nums[j] === target) {
+//         answer.push(i, j);
+//         return answer;
+//       }
+//     }
+//   }
+// }
+const findSumOfTwoWithHashTable = function (nums, target) {
+  const ANSWER = [];
+  const NUM_LEN = nums.length;
+  const hashTable = {};
+
+  for (let i = 0; i < NUM_LEN; i += 1) {
+    const check = hashTable.hasOwnProperty(target - nums[i]);
+    if (check) {
+      ANSWER.push(hashTable[target - nums[i]], i);
+      break;
+    } else {
+      hashTable[nums[i]] = i;
     }
   }
-}
+  return ANSWER;
+};
 
-console.log(findSumOfTwo([2, 7, 10, 19], 17));
+console.log(findSumOfTwoWithHashTable([2, 7, 10, 19], 9));
