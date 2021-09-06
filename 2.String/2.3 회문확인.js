b6c3c6b281d92cc5f5ad136e7bf54dd2d89a7b6f
jungeun0121 <dodamdodam121@gmail.com>
2021-09-06 19:44:13 +0900

    2.3

---
+++ 2.String/2.3 회문확인.js
@@ -0,0 +1,36 @@
+// 입력값을 거꾸로 읽었을 때, 동일한지 여부에 따라 true, false 반환
+// 알파벳, 숫자를 제외한 모든 문자와 공백은 제거
+// 알파벳은 대소문작 구분 X
+
+// test case
+// const INPUT = "   Abbc, c, bb, a    ";
+// const INPUT = ["ab", "cd", "ef"];
+// const INPUT = ["abc", "bca", "cba"];
+const INPUT = ["11/22", "aBb-A", "--,","22_11"]
+
+
+const solution = function(input) {
+
+    //  string이 아닌 type은 string으로 형 변환
+    if (typeof(input) != "string") {input = input.toString()};
+    
+    // 알파벳과 숫자를 제외한 모든 문자 제거 후, 소문자 변환
+    const filteredString = input.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    console.log("필터링 된 문자 : ", filteredString)
+    
+    // 불필요한 문자를 제거한 
+    const filteredStringReverse = filteredString.split("").reverse().join("")
+    console.log("뒤집은 문자 : ", filteredStringReverse)
+
+    // 회문인지 확인하여 반환
+    return filteredString === filteredStringReverse
+
+};
+
+
+
+const start_time = new Date();
+const ANSWER = solution(INPUT);
+console.log(ANSWER)
+const end_time = new Date();
+console.log("running time : ",end_time - start_time); 
