bca0cf47ecc8ebb194b91160fadec3ed9ced111d
chanwoo <chanwoo@chanwooui-MacBookAir.local>
2021-09-08 19:27:25 +0900

    2.4 not done

---
+++ 2.String/2.4.js
@@ -0,0 +1,62 @@
+/*
+문제: 문자열 리스트가 주어지는데 리스트에 있는 문자열을 검사해 서로 같은 애너그램을 가지는
+문자열 그룹으로 묶어보자. 예를 들어 문자열 리스트가 아래와 같이 주어졌다고 가정해보자.
+strs = ['eat', 'repaid', 'paired' ,  'tea', 'bat']로 주어졌다면 다음과 같은 에너그램을 모아 2차원 리스트로 반환해야 한다.
+res = [
+    ['eat,'tea'],
+    ['repaid','paired'],
+    ['bat'];
+]
+*/
+
+const gStrs = ['eat', 'repaid', 'paired' , 'tea', 'bat'];
+
+result = (strs) =>{
+    let res = [];
+    strs.sort();
+    // const tempObj = {};
+
+    for(let i = 0 ; i < strs.length ; i++){
+        const iLength = strs[i].length;
+        res.push([strs[i]]);
+
+        for(let j = 0 ; j < strs.length ; j++){
+            if(j !== 0 && strs[i].length !== strs[j].length) return;
+            if(i !== j){
+
+                const set = new WeakSet();
+                set.add(strs[i].split(""));
+                set.add(strs[j].split(""));
+
+                if(set.size !== 1){
+                    res[i].push(strs[j]);
+                    strs.splice(j,1);
+                }
+
+            }
+        }
+        strs.splice(i,1);
+
+    }
+
+
+
+
+
+    //
+    // for(let i = 0 ; i < strs.length ; i++){
+
+    //     if(tempObj[strs[i].length]){
+    //         tempObj[strs[i].length].push(strs[i])
+    //     }else{
+    //         tempObj[strs[i].length]= [strs[i]];
+    //     }
+    // }
+    
+
+    // console.log(tempObj);
+
+    return res;
+}
+
+console.log(result(gStrs));
