d32c358126288b73d74feef35d46d2d1f090fefc
Seung-wan <tmddhks0104@gmail.com>
2021-09-15 19:52:07 +0900

    update testcases

--- 3.Linked_List/5.js
+++ 3.Linked_List/5.js
@@ -3,17 +3,14 @@
 function Node(value) {
   return { value: value, next: null };
 }
-
 const checkCycle = function (node) {
   let head = node;
   let nodeSet = new Set();
 
-  while (head !== null) {
+  while (head) {
     if (nodeSet.has(head)) {
-      // head가 set에 있으면
       return true;
     }
-    // set에 head 추가
     nodeSet.add(head);
     head = head.next;
   }
@@ -21,16 +18,20 @@ const checkCycle = function (node) {
   return false;
 };
 
-let root = Node('10');
-let nodeB = (root.next = Node('11'));
-let nodeC = (nodeB.next = Node('12'));
-let nodeD = (nodeC.next = Node('13'));
+let root = Node('1');
+let nodeB = (root.next = Node('2'));
+let nodeC = (nodeB.next = Node('3'));
+nodeC.next = root;
 
-let root2 = Node('5');
-let nodeE = (root2.next = Node('11'));
-let nodeF = (nodeE.next = Node('12'));
-let nodeG = (nodeF.next = Node('13'));
-nodeG.next = nodeF;
+let root2 = Node('1');
+
+let root3 = Node('1');
+let nodeE = (root3.next = Node('2'));
+let nodeF = (nodeE.next = Node('3'));
+let nodeG = (nodeF.next = Node('4'));
+let nodeH = (nodeG.next = Node('5'));
+let nodeI = (nodeH.next = Node('6'));
 
 console.log(checkCycle(root));
 console.log(checkCycle(root2));
+console.log(checkCycle(root3));
